12776d20dc4aeb900a90e57400aa17408d534fa2
sanjayprasad s <sanjayprasads@sanjayprasads-MacBook-Air.local>
2022-06-09 23:20:07 +0530

    first commit try2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
 
 function App() {
+  //let currentList ;
+
+  let [currentList, changeList] = React.useState(["hello", "sss"]);
+  let [input, changeinput] = React.useState("");
+  // function changeList(x){
+  //   return currentList.append(x);
+  // }
+
+  function changeInputHandler(e) {
+    changeinput(e.target.value);
+  }
+  function addList() {
+    changeList([...currentList, input]);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>the following app is a todo list </p>
+        <p>To do list :</p>
+        {/* <form> */}
+        <input
+          type="text"
+          id="fname"
+          name="fname"
+          value={input}
+          onChange={(e) => changeInputHandler(e)}
+        ></input>{" "}
+        <br></br>
+        <button onClick={addList}>Submit</button>
+        {/* <input type="submit" value="Submit"></input> */}
+        {/* </form> */}
+        <p>List below :</p>
+        <ul>
+          {currentList &&
+            currentList.map((x, index) => <li key={index}>{x}</li>)}
+        </ul>
       </header>
     </div>
   );
